0dc91fff708fad903b21cbb05d2aa7a0c2567fdd
AdamP501 <axonp2026@student.cis.edu.hk>
2023-09-13 09:10:08 +0800

    finished the code

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.print("Enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        int age2 = (age/2) + 7;
+        System.out.print(age + "-year olds should date someone that is atleast " + age2 + " years old.");
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        System.out.print("Enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        int page = 100 - age;
+        System.out.print(age + "-year olds should read atleast " + page + " pages before giving up on a book");
     }
 }
